707f6e763b21c858dee994510b6479907510ca49
tardisgallifrey <bigmanrunning@gmail.com>
2025-04-14 13:12:23 -0500

    First theGame commit 04/14/25

---
+++ src/main/java/com/tardisgallifrey/thegame/Main.java
@@ -0,0 +1,12 @@
+package com.tardisgallifrey.thegame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Let the Game begin");
+
+        //TODO: Game class
+        //TODO: Combat class
+        //TODO: Movement class
+    }
+}
