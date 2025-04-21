6781adabf7bac51095fab59c0d5b02f3a7d53012
Dave <bigmanrunning@gmail.com>
2025-04-21 13:14:11 +0000

    Change attribute from array to Recod 04/20/25

---
+++ src/main/java/com/tardisgallifrey/thegame/Attributes.java
@@ -0,0 +1,7 @@
+package com.tardisgallifrey.thegame;
+
+public record Attributes(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) {
+
+    
+
+}
--- src/main/java/com/tardisgallifrey/thegame/Person.java
+++ src/main/java/com/tardisgallifrey/thegame/Person.java
@@ -4,19 +4,24 @@ import java.util.ArrayList;
 
 public class Person {
 
-    public String name;
-    public int classtype;
-    public String species;
-    public int armorClass;
-    public int hitPoints;
-    public Dice hitDice;
-    public int strength;
-    public int dexterity;
-    public int constitution;
-    public int intelligence;
-    public int wisdom;
-    public int charisma;
-    public ArrayList<Integer> attributes;
+    private String name;
+    private int classtype;
+    private String species;
+    private int armorClass;
+    private int hitPoints;
+    private Dice hitDice;
+    private Attributes attributes;
+   
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public Person(String name, int[] attributes)
+    {
+        this.name = name;
+        this.attributes = new Attributes(attributes[0], attributes[1], attributes[2], attributes[3], attributes[4], attributes[5]);
+    }
 
 
 }
